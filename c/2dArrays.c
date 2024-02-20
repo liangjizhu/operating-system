f53ae29eba68ae3a571cd6ca8302d2682be6b3e8
Liang Ji Zhu <www.1159068228@gmail.com>
2024-02-21 00:20:40 +0100

    2darray

--- c/2dArrays.c
+++ c/2dArrays.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
 
 int main(){
-    double prices[] = {5.0, 10.0, 15.0, 25.0, 20.0};
-    printf("%d bytes\n", sizeof(prices));
-    for (int i = 0; i < sizeof(prices) / sizeof(prices[0]); i++)
+    // int numbers[2][3] = {
+    //                     {1, 2, 3}, 
+    //                     {4, 5, 6}
+    //                     };
+
+    int numbers[3][3];
+
+    int rows = sizeof(numbers)/sizeof(numbers[0]);
+    int cols = sizeof(numbers[0])/sizeof(numbers[0][0]);
+
+    printf("rows: %d\n", rows);
+    printf("cols: %d\n", cols);
+
+    numbers[0][0] = 1;
+    numbers[0][1] = 2;
+    numbers[0][2] = 3;
+    numbers[1][0] = 4;
+    numbers[1][1] = 5;
+    numbers[1][2] = 6;
+    numbers[2][0] = 7;
+    numbers[2][1] = 8;
+    numbers[2][2] = 9;
+
+    for (int i = 0; i < rows; i++)
     {
-        printf("%.2lf\n", prices[i]);
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d ", numbers[i][j]);
+        }
+        printf("\n");
     }
     
 
